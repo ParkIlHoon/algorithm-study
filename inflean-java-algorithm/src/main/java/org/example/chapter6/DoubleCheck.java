aa4e176a9e5ef588b8e981d240388fca0512a102
박일훈 <chiwoo2074@gmail.com>
2023-01-07 16:09:25 +0900

    5. 중복 확인

---
+++ inflean-java-algorithm/src/main/java/org/example/chapter6/DoubleCheck.java
@@ -0,0 +1,28 @@
+package org.example.chapter6;
+
+import java.util.Scanner;
+
+public class DoubleCheck {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = sc.nextInt();
+        int[] students = new int[cnt];
+        for (int i = 0; i < cnt; i++) {
+            students[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(students));
+    }
+
+    private static String solution(int[] students) {
+        for (int i = 0; i < students.length; i++) {
+            for (int j = i+1; j < students.length; j++) {
+                if (students[i] == students[j]) return "D";
+            }
+        }
+
+        return "U";
+    }
+}
